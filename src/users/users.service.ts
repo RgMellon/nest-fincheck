7fcd3cb72834dc187d37959bd96e041b931d52eb
RgMellon <renan_gian@hotmail.com>
2025-02-05 08:32:32 -0300

    ⚡️[feat] : add prisma service and create user module

---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
+
+    return user;
+  }
+}
