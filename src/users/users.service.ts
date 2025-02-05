16a406121abdad2c3f78c5a94cbf80b27b80a91b
RgMellon <renan_gian@hotmail.com>
2025-02-05 13:33:25 -0300

    [feat] : add prisma service to create users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const emailTaken = this.prismaService.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (emailTaken) throw new ConflictException('This email already taken');
+
+    const hashedPassword = await hash(createUserDto.password, 12);
+
     const user = await this.prismaService.user.create({
       data: {
         ...createUserDto,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              { name: 'Salário', icon: 'travel', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+
+              { name: 'Casa', icon: 'home', type: 'EXPENSIVE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSIVE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSIVE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSIVE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSIVE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSIVE' },
+            ],
+          },
+        },
       },
     });
 
